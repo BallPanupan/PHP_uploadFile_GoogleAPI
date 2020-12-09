61ad87ad8974f7cf8f06731af86d632112116e4e
Panupan Tantiwacharapan <ball085573@hotmail.com>
2020-12-09 17:22:33 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Upload to Google Drive</title>
+  </head>
+  <body>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+      Select image to upload:
+      <input type="file" name="filUpload" id="filUpload">
+      <input type="submit" value="Upload Image" name="submit">
+    </form>
+  </body>
+</html>
